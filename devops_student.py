e4743fb8e701eca6eae9b89271b75ee1d806c1cc
hmalak <hmalak@spartaglobal.com>
2020-07-06 09:19:57 +0100

    ---

--- devops_student.py
+++ devops_student.py
@@ -1,11 +1,13 @@
+# Imports the student_data
 from student_data import *
 
-class DevOPs(Student):
+class DevOPs(Student): # inherits characteristics from Student
     def __init__(self, name, city, classes, sql):
         super().__init__(name, city, classes)
-        self.sql = sql
+        self.sql = sql # unique to devops only
 
-    def printdevops(self):
+
+    def printdevops(self): # method is unique to Person
         self.printvalues()
         print("You have learned " + self.sql)
 
--- student_data.py
+++ student_data.py
@@ -1,9 +1,12 @@
+# parent class
 class Student:
+    # method initializes the variables
     def __init__(self, name, city, classes):
         self.name = name
         self.__city = city
         self.classes = classes
 
+    # method prints out the variable
     def printvalues(self):
         print("Your name is " + self.name)
         print("Your name is " + self.__city)
